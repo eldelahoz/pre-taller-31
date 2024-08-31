387fbbdcfa6f4a0f3399595428de0fac35da3e42
eldelahoz <andelahoz13@gmail.com>
2024-08-31 11:39:13 -0500

    ADD: Modelo status configurado

--- app/statu/models.py
+++ app/statu/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Status(models.Model):
+    statu_id = models.AutoField(primary_key=True)
+    statu_name = models.CharField(max_length=100, blank=False, null=False, unique=True)
