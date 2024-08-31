b60387b6fdd0df3c1bd5d50a485745059f1c2e65
rina plata <rinamarcelaplata@gmail.com>
2024-08-31 12:31:16 -0500

    add: create model book

--- app/book/models.py
+++ app/book/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from ..author.models import Authors
 
 # Create your models here.
+class Books(models.Model):
+    book_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=200)
+    author_id = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    publication_date = models.DateField()
+    genre_id = models.ForeignKey(Genre, on_delete=models.SET_NULL, null=True)
+    status = models.CharField(max_length=20)
+
+    
+    def __str__(self):
+        return f"{self.title} {self.genre_id} {self.status}"
