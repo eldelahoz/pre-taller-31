5042d2bd308a8afd153f45ebe2ce7f57d7f338fd
rina plata <rinamarcelaplata@gmail.com>
2024-08-31 11:44:32 -0500

    add: create model author

--- app/author/models.py
+++ app/author/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Authors(models.Model):
+    author_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
